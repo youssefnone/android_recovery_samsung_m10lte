7fdfeae5eb8c66b1107e6e4d143085525ce3bbf0
claudwait <youssefnone096@gmail.com>
2023-03-01 14:07:30 +0100

    initial tree for ofox android 11/12.1

---
+++ twrp_m10lte.mk
@@ -0,0 +1,28 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from our custom product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/dt.img:boot.img
+
+PRODUCT_DEVICE := m10lte
+PRODUCT_NAME := twrp_m10lte
+PRODUCT_MODEL := Samsung Galaxy M10
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
